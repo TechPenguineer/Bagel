2b5419584809c405e292c43ec468e9c3ce7e2101
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2022-07-17 18:19:48 -0400

    added basic grammar

---
+++ src/lang/SYNTAX.py
@@ -0,0 +1,34 @@
+class SYNTAX:
+    DEFINE_PROPERTY = "-"
+    SPECIFY_WINDOW = "$"
+    DEFINE_FILE_PATH = "%="
+    DEFINE_EOL = ":"
+    DEFINE_COMMENT_LINE = "!#"
+    DEFINE_NEW_BLOCK_START = "{"
+    DEFINE_NEW_BLOCK_END = "}"
+
+class OPERATORS:
+    SET_VALUE = "="
+    ADD_VALUE = "+="
+    SUB_VALUE = "-="
+    MUL_VALUE = "*="
+    DIV_VALUE = "/="
+    MOD_VALUE = "%="
+    INCREMENT = "++"
+    DECREMENT = "--"
+    EQUAL = "=="
+    NOT_EQUAL = "!="
+    GREATER_THAN = ">"
+    LESS_THAN = "<"
+    GREATER_THAN_OR_EQUAL = ">="
+    LESS_THAN_OR_EQUAL = "<="
+    AND = "&&"
+    OR = "||"
+class BLOCK_TYPE:
+    CONTENT_BLOCK = "container"
+    
+class MESSUREMENTS:
+    PIXELS = "px"
+    PERCENT = "%"
+    VW = "vw"
+    VH = "vh"
