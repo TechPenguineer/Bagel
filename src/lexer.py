eb1cd7b19eb8f54d29f909a71c6d4f0287d48722
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2022-07-17 23:33:04 -0400

    convert window property to html

--- src/lexer.py
+++ src/lexer.py
@@ -4,4 +4,6 @@ def getListOfLines(data):
     return data.split("\n")
 
 def lexer(lines):
-    getWindowProperty.getWindowPropertyLexer(lines, GRAMMAR.SYNTAX.DEFINE_WINDOW_PROPERTY)
+    wp=getWindowProperty.getWindowPropertyLexer(lines, GRAMMAR.SYNTAX.DEFINE_WINDOW_PROPERTY)
+    x=getWindowProperty.returnWindowPropertyAsHtml(wp[0], "hello world")
+    print(x)
--- src/lib/getWindowProperty.py
+++ src/lib/getWindowProperty.py
@@ -1,5 +1,12 @@
+
+
 def removeWhitespaceFromReader(dataList):
-    return ' '.join(dataList).split()
+    dataList = [i for i in dataList if i]
+    return dataList
+
+def acceptStringData(dataList):
+    quotes = '"'
+    dataList.split(quotes)
 
 def getWindowPropertyLexer(lines, windowPropertyIndicator):
 
@@ -12,7 +19,14 @@ def getWindowPropertyLexer(lines, windowPropertyIndicator):
         tokensOnLine = [removeWhitespaceFromReader(cL)]
         #print(tokensOnLine)
         for token in cL:
-           onToken += 1
-           if token == windowPropertyIndicator:
+            onToken += 1
+            if token == windowPropertyIndicator:
                  propertyToken = onToken
                  print( "Window Property Set on line: " + str(lineNumb) + ": Property of " + str(tokensOnLine[0][ onToken ] ))
+                 return [str(tokensOnLine[0][ onToken ])]
+
+def returnWindowPropertyAsHtml(propertyType, value):
+    if propertyType == "title":
+        return "<title>" + value + "</title>"
+    if propertyType == "ico":
+        return "<link rel='icon' href='" + value + "'>"
