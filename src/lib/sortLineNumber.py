c6225aacd8ed9adc1e4019c37b00775443bfa041
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2022-07-18 15:58:57 -0400

    sort by line number

---
+++ src/lib/sortLineNumber.py
@@ -0,0 +1,11 @@
+def sortLineNumbers(data, lineNumbers):
+    def natural_key(string_):
+     try:
+        import re
+     except ImportError:
+        print("Please install the 're' module.")
+        return
+     return [int(s) if s.isdigit() else s for s in re.split(r'(\d+)', string_) if s]  
+
+    x = sorted(data, key=natural_key)
+    return x
