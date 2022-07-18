8c0db8b4f6c1f2b29e5f9054c2892059caea2f56
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2022-07-17 22:32:40 -0400

    fixed reading white spaces

--- src/lib/getWindowProperty.py
+++ src/lib/getWindowProperty.py
@@ -1,3 +1,6 @@
+def removeWhitespaceFromReader(dataList):
+    return ' '.join(dataList).split()
+
 def getWindowPropertyLexer(lines, windowPropertyIndicator):
 
     # Finds Tokens in the lines
@@ -6,10 +9,10 @@ def getWindowPropertyLexer(lines, windowPropertyIndicator):
         lineNumb += 1
         cL = line.split(" ")
         onToken = 0
-        propertyToken = 0
-        tokensOnLine = [cL]
+        tokensOnLine = [removeWhitespaceFromReader(cL)]
+        #print(tokensOnLine)
         for token in cL:
-            onToken += 1
-            if token == windowPropertyIndicator:
-                propertyToken = onToken
-                print( "Window Property Set on line: " + str(lineNumb) + ": Property of " + str(tokensOnLine[0][ onToken ] ))
+           onToken += 1
+           if token == windowPropertyIndicator:
+                 propertyToken = onToken
+                 print( "Window Property Set on line: " + str(lineNumb) + ": Property of " + str(tokensOnLine[0][ onToken ] ))
